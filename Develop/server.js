96a55552b17fd64369432d0f803f333e5f5f9509
Ani-Kandikattu <120747994+Ani-Kandikattu@users.noreply.github.com>
2023-04-12 21:42:54 -0700

    added middleware and started get methods for server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,17 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const notes = require("./db/db.json");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes.slice(1));
+});
+
