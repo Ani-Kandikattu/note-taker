2483d335aeda4d01b87652d71a7f56262cccbbd4
Ani-Kandikattu <120747994+Ani-Kandikattu@users.noreply.github.com>
2023-04-12 22:06:30 -0700

    finished get methods

--- Develop/server.js
+++ Develop/server.js
@@ -15,3 +15,10 @@ app.get("/api/notes", (req, res) => {
   res.json(notes.slice(1));
 });
 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
