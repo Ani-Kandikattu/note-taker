26d03d8c90ad1f4eb73a877a8f7cf0501608245a
Ani-Kandikattu <anikandikattu@gmail.com>
2023-04-14 09:48:16 -0700

    added comments, made a small change to one of the get methods, added a listen method and started to create a function to create note objects

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,25 @@
+// Importing node modules
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+
+// Creating a new instance of express and assigning it to app
 const app = express();
 
+// This will set PORT variable to 3001 if there is no other value
 const PORT = process.env.PORT || 3001;
 
+// This is to import db.json file for storing and retrieving notes
 const notes = require("./db/db.json");
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Route Handlers
+
+// GET Methods
 app.get("/api/notes", (req, res) => {
   res.json(notes.slice(1));
 });
@@ -19,6 +28,14 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body;
+})
+
+function createNoteObject()
+
+app.listen(port, () => console.log(`Listening on port ${PORT}!`));
