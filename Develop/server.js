086420f5d602db6c1e5ade881beaaa46ef768b15
Ani-Kandikattu <anikandikattu@gmail.com>
2023-04-14 15:19:30 -0700

    finished function to create note object and add it to an array, then write the array to db.json, also fixed POST method

--- Develop/server.js
+++ Develop/server.js
@@ -32,10 +32,32 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// Function to create a new note object with body parameter
+function createNoteObject(body, notes) {
+
+  const note = body;
+
+  // Initializes a notes array to store the note object
+  notesArray = [];
+
+  // Sets the id for the new note to the first element of the notesArray and increments by 1 to ensure each note has a unique id.
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  // To push the new note to the array
+  notesArray.push(note);
+
+  // To write updated notesArray to db.json with 2 spaces for indentation
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(notesArray, null, 2)
+  )
+}
+
+// POST Method
 app.post('/api/notes', (req, res) => {
-  const note = req.body;
+  const note = createNoteObject(req.body, notes);
+  res.json(note)
 })
 
-function createNoteObject()
-
-app.listen(port, () => console.log(`Listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
